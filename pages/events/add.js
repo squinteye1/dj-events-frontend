de2d26d2848d07ec55f2c0404fb6a0fbb2f4d31a
squinteye1 <neil.d.mckay@googlemail.com>
2022-03-07 22:35:17 +0000

    Pages and Routing

---
+++ pages/events/add.js
@@ -0,0 +1,5 @@
+export default function AddEventPage() {
+  return (
+    <h1>Add Event</h1>
+  )
+}
---
+++ pages/events/index.js
@@ -0,0 +1,5 @@
+export default function EventsPage() {
+  return (
+    <h1>My Events</h1>
+  )
+}
